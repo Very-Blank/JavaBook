00ca545afa7b7a9b71694dba3c25fa8aab7ad082
Very-Blank <aapeli.saarelainen.76@gmail.com>
2025-04-15 11:21:30 +0300

    Change

--- src/main/java/ui/PhoneNumber.java
+++ src/main/java/ui/PhoneNumber.java
@@ -15,19 +15,22 @@ import javafx.event.*;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 
-public class PhoneNumber extends HBox{
+public class PhoneNumber extends HBox {
     private FuckFxMenuButton countrySelector;
     private String selectedCountryCode;
     private TextField textField;
 
-    public PhoneNumber(Assets assets){
+    public PhoneNumber(String phoneNumber, String countryCode, Assets assets) {
         super();
-        String[] countryCodes = new String[]{"+358", "+1", "+46", "+81", "+82"};
+        String[] countryCodes = new String[] { "+358", "+1", "+46", "+81", "+82" };
+        this.textField = new TextField();
+        this.selectedCountryCode = countryCode;
+        this.countrySelector = new FuckFxMenuButton(countryCode);
 
-        this.selectedCountryCode = countryCodes[0];
-        this.countrySelector = new FuckFxMenuButton(countryCodes[0]);
+        this.textField.setText(phoneNumber);
 
-        this.countrySelector.setMenuBackground(new Background(new BackgroundFill(Color.valueOf(assets.gray2), null, null)));
+        this.countrySelector
+                .setMenuBackground(new Background(new BackgroundFill(Color.valueOf(assets.gray2), null, null)));
         this.countrySelector.setMenuItemBackground(assets.elevated);
         this.countrySelector.setMenuItemFontColor(assets.textColor);
         this.countrySelector.setMenuItemFont(assets.fonts.normal);
@@ -38,20 +41,19 @@ public class PhoneNumber extends HBox{
 
         for (int i = 0; i < countryCodes.length; i++) {
             final String code = countryCodes[i];
-            FuckFxMenuItem countryCode = new FuckFxMenuItem(code);
-            this.countrySelector.addMenuItem(countryCode);
-            countryCode.setOnAction((_) -> {
+            FuckFxMenuItem menuItem = new FuckFxMenuItem(code);
+            this.countrySelector.addMenuItem(menuItem);
+            menuItem.setOnAction((_) -> {
                 countrySelector.setText(code);
                 countrySelector.closePopup();
                 selectedCountryCode = code;
             });
         }
 
-        textField = new TextField();
-        textField.textProperty().addListener(new ChangeListener<String>() {
+        this.textField.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue,
-                String newValue) {
+                    String newValue) {
                 if (!newValue.matches("\\d*")) {
                     textField.setText(newValue.replaceAll("[^\\d]", ""));
                 }
@@ -61,8 +63,7 @@ public class PhoneNumber extends HBox{
         super.getChildren().addAll(countrySelector, textField);
     }
 
-    public String getPhoneNumber(){
+    public String getPhoneNumber() {
         return this.selectedCountryCode + " " + textField.getText();
     }
 }
-
